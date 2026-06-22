17481f9663f2c9778f22f24c69845913c7610d71
Eitish <tyagieitish2011.com>
2026-06-22 18:19:30 +0530

    Test auto deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,7 +50,7 @@ function App() {
 
       {/* Header */}
       <div className="header">
-        <h1>📋 DevBoard Pro</h1>
+        <h1>📋 DevBoard Pro 1</h1>
         <p>Apne tasks manage karo</p>
       </div>
 
